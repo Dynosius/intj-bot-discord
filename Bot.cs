7342a8421a196713c3cc032c4a938ef94b7102cd
Dynosius <dinokraljmusic@gmail.com>
2018-09-21 23:20:19 +0200

    Initial commit

---
+++ Bot.cs
@@ -0,0 +1,69 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace INTJBot
+{
+    class Bot
+    {
+        private string authToken;
+        private DiscordSocketClient client;
+        private CommandService commands;
+        private IServiceProvider services;
+        public Bot(string auth)
+        {
+            authToken = auth;
+        }
+        public async Task Start()
+        {
+            client = new DiscordSocketClient();
+            commands = new CommandService();
+
+            client.Log += LogAsync;
+            services = new ServiceCollection()
+                    .BuildServiceProvider();
+            await InstallCommands();
+
+            await client.LoginAsync(TokenType.Bot, authToken);
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+        public async Task InstallCommands()
+        {
+            // Hook the MessageReceived Event into our Command Handler
+            client.MessageReceived += HandleCommand;
+            // Discover all of the commands in this assembly and load them.
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        public async Task HandleCommand(SocketMessage messageParam)
+        {
+            // Don't process the command if it was a System Message
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+            // Create a number to track where the prefix ends and the command begins
+            int argPos = 0;
+            // Determine if the message is a command, based on if it starts with '!' or a mention prefix
+            if (!(message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(client.CurrentUser, ref argPos))) return;
+            // Create a Command Context
+            var context = new CommandContext(client, message);
+            // Execute the command. (result does not indicate a return value, 
+            // rather an object stating if the command executed successfully)
+            var result = await commands.ExecuteAsync(context, argPos, services);
+            if (!result.IsSuccess)
+                await context.Channel.SendMessageAsync(result.ErrorReason);
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace INTJBot
+{
+    class Program
+    {
+        // STAThread has to be used because OpenFileDialog had issues running on a static thread
+        // Required for accessing OLE functions
+        [STAThread]
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter bot auth token: ");
+            string auth = GetBotToken(OpenFileChooser());
+
+            new Bot(auth).Start().GetAwaiter().GetResult();
+        }
+        
+        private static string OpenFileChooser()     // This method is used for choosing a file which holds a token
+        {
+            string filename = "";
+            using (OpenFileDialog openFileDialog1 = new OpenFileDialog())
+            {
+                openFileDialog1.InitialDirectory = "c:\\";
+                openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog1.FilterIndex = 2;
+                openFileDialog1.RestoreDirectory = true;
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    filename = openFileDialog1.FileName;
+                }
+            }
+            return filename;
+        }
+
+        // Method reads the first line from the file. Assuming you have the token string as the only line in there
+        private static string GetBotToken(string filename) 
+        {
+            string token = "";
+            if (File.Exists(filename))
+            {
+                token = File.ReadLines(filename).First();
+            }
+            return token;
+        }
+    }
+
+}
